114939e85850ea5d9419a216be8fc5ffe1c3ac5c
fandrei <andrei_f_11@mail.ru>
2011-11-10 17:44:14 +0700

    init

---
+++ AppMetrics.DataCollector/LogEvent.ashx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace AppMetrics.DataCollector
+{
+	/// <summary>
+	/// Summary description for LogEvent
+	/// </summary>
+	public class LogEvent : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			try
+			{
+				var sessionId = context.Request.Params["Session"];
+				var rootPath = context.Request.PhysicalApplicationPath;
+				var time = DateTime.UtcNow.ToString("u");
+				time = time.Replace(':', '_');
+				var fileName = string.Format("{0}\\Data\\{1}.{2}.txt", rootPath, time, sessionId);
+
+				var logData = context.Request.Params["Data"];
+
+				using (var writer = new StreamWriter(fileName, true, Encoding.Unicode))
+				{
+					writer.Write(logData);
+				}
+			}
+			catch (Exception exc)
+			{
+				Trace.WriteLine(exc);
+			}
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return true;
+			}
+		}
+	}
+}
---
+++ DebugProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace DebugProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				var url = "http://localhost:51379/LogEvent.ashx";
+				var vals = new NameValueCollection { { "Session", Guid.NewGuid().ToString() }, { "Data", DateTime.Now.ToString() } };
+				using (var client = new WebClient())
+				{
+					var response = client.UploadValues(url, "POST", vals);
+					var responseText = Encoding.ASCII.GetString(response);
+					Console.WriteLine(responseText);
+				}
+			}
+			catch (Exception exc)
+			{
+				Console.WriteLine(exc);
+			}
+		}
+	}
+}
