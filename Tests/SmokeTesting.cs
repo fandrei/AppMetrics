f21797883e3e3c0885b9442e74bd6daa55cb359f
fandrei <andrei_f_11@mail.ru>
2011-11-28 21:03:10 +0700

    added skeleton of smoke test

---
+++ Tests/SmokeTesting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AppMetrics.Client;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class SmokeTesting
+	{
+		[Test]
+		public void SmokeTest()
+		{
+			var tracker = new Tracker("http://184.73.228.71/AppMetrics/LogEvent.ashx", "SmokeTest");
+			tracker.Log("TestMessage", "TestValue");
+
+			Tracker.Terminate(true);
+		}
+	}
+}
