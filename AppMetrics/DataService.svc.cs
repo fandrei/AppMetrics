fd0bed3066296e7c67a2d2bc7a53ab2ab967bbdb
fandrei <andrei_f_11@mail.ru>
2011-12-13 20:40:00 +0700

    refactoring

--- AppMetrics/DataService.svc.cs
+++ AppMetrics/DataService.svc.cs
@@ -5,6 +5,7 @@ using System.Data.Services.Common;
 using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
+using System.ServiceModel;
 using System.ServiceModel.Web;
 using AppMetrics.DataModel;
 
@@ -20,12 +21,21 @@ namespace AppMetrics
 
 			config.SetEntitySetAccessRule("Sessions", EntitySetRights.All);
 			config.SetEntitySetAccessRule("Records", EntitySetRights.All);
+
+			config.SetServiceOperationAccessRule("GetSessions", ServiceOperationRights.AllRead);
 		}
 
 		protected override void HandleException(HandleExceptionArgs args)
 		{
 			base.HandleException(args);
 			Trace.WriteLine(args.Exception);
+		}
+
+		[WebGet]
+		public Session[] GetSessions(string appKey, string period)
+		{
+			var periodSpan = TimeSpan.Parse(period);
+			return DataSource.GetSessions(appKey, periodSpan).ToArray();
 		}
 	}
 }
