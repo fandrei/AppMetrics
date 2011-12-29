a4fc6ef6e50e72881515fbdbd10206902afd686f
fandrei <andrei_f_11@mail.ru>
2011-12-29 19:14:21 +0700

    added simple tool to find median and quartiles for overall input

---
+++ DataConvertor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AppMetrics.DataModel;
+
+namespace AppMetrics.DataConvertor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				if (args.Length != 1)
+					throw new ApplicationException("Invalid command line args");
+
+				var dataPath = args[0];
+
+				var records = DataSource.GetRecordsFromPath(dataPath, DateTime.Now - DateTime.MinValue);
+
+				var latencies = new List<decimal>();
+
+				foreach (var record in records)
+				{
+					if (record.Name.StartsWith("Latency"))
+					{
+						var val = decimal.Parse(record.Value);
+						latencies.Add(val);
+					}
+				}
+
+				decimal median, lowerQuartile, upperQuartile;
+				Stats.FindQuartiles(latencies, out median, out lowerQuartile, out upperQuartile);
+			}
+			catch (Exception exc)
+			{
+				Console.WriteLine(exc);
+			}
+		}
+	}
+}
---
+++ DataConvertor/Stats.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppMetrics.DataConvertor
+{
+	static class Stats
+	{
+		public static void FindQuartiles(IEnumerable<decimal> vals,
+			out decimal median, out decimal lowerQuartile, out decimal upperQuartile)
+		{
+			var sorted = new List<decimal>(vals);
+			sorted.Sort();
+
+			var medianIndex = GetMedianIndex(sorted.Count) - 1;
+			median = FindValue(sorted, medianIndex);
+
+			var lowerQuartileIndex = GetMedianIndex(Math.Floor(medianIndex) + 1) - 1;
+			lowerQuartile = FindValue(sorted, lowerQuartileIndex);
+
+			var upperQuartileIndex = Math.Ceiling(medianIndex) + GetMedianIndex(sorted.Count - Math.Ceiling(medianIndex)) - 1;
+			upperQuartile = FindValue(sorted, upperQuartileIndex);
+		}
+
+		private static decimal GetMedianIndex(decimal count)
+		{
+			return (count + 1) / 2;
+		}
+
+		static decimal FindValue(IList<decimal> vals, decimal i)
+		{
+			var rounded = Math.Floor(i);
+			if (i == rounded)
+				return vals[(int)rounded];
+			else
+				return (vals[(int)rounded] + vals[(int)rounded + 1]) / 2;
+		}
+	}
+}
