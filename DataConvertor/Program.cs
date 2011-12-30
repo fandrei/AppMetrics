b617d7770a2ed75bc5a9e39f30eebec6ad3e9dd4
fandrei <andrei_f_11@mail.ru>
2011-12-30 12:55:09 +0700

    added time measuring

--- DataConvertor/Program.cs
+++ DataConvertor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -17,15 +18,22 @@ namespace AppMetrics.DataConvertor
 					throw new ApplicationException("Invalid command line args");
 
 				var dataPath = args[0];
+				var watch = Stopwatch.StartNew();
 
 				var records = DataSource.GetRecordsFromPath(dataPath, DateTime.Now - DateTime.MinValue);
+
+				Console.WriteLine("Parsing: {0} secs", watch.Elapsed.TotalSeconds);
 
 				var latencies = (from record in records
 								 where record.Name.StartsWith("Latency")
 								 select decimal.Parse(record.Value)).ToList();
 
+				Console.WriteLine("Preparing data: {0} secs", watch.Elapsed.TotalSeconds);
+
 				decimal median, lowerQuartile, upperQuartile, min, max;
 				Stats.CalculateSummaries(latencies, out median, out lowerQuartile, out upperQuartile, out min, out max);
+
+				Console.WriteLine("Finding statistic summaries: {0} secs", watch.Elapsed.TotalSeconds);
 			}
 			catch (Exception exc)
 			{
