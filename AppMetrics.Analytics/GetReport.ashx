4813523cf090e10e8829af672842a7f3b9b82c7e
fandrei <andrei_f_11@mail.ru>
2012-01-17 17:04:24 +0700

    added report page stub

---
+++ AppMetrics.Analytics/GetReport.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="GetReport.ashx.cs" Class="AppMetrics.Analytics.GetReport" %>
---
+++ AppMetrics.Analytics/GetReport.ashx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace AppMetrics.Analytics
+{
+	/// <summary>
+	/// Creates reports and delivers to the user
+	/// </summary>
+	public class GetReport : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			InitProcessing();
+
+			context.Response.ContentType = "text/plain";
+			lock (Sync)
+			{
+				context.Response.Write(_reportText);
+			}
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		static void InitProcessing()
+		{
+			lock (Sync)
+			{
+				if (_workerThread == null)
+				{
+					_workerThread = new Thread(ThreadStart);
+					_workerThread.Start();
+				}
+			}
+		}
+
+		static void ThreadStart()
+		{
+			while (true)
+			{
+				try
+				{
+					CreateReport();
+					Thread.Sleep(100);
+				}
+				catch (ThreadAbortException)
+				{
+					break;
+				}
+				catch (Exception exc)
+				{
+					Trace.WriteLine(exc);
+				}
+			}
+		}
+
+		static void CreateReport()
+		{
+			
+		}
+
+		private static Thread _workerThread;
+		private static readonly object Sync = new object();
+		private static string _reportText = "";
+	}
+}
