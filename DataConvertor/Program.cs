377f4b0ec5c641424d4180def0cf1cc0d8f1798c
fandrei <andrei_f_11@mail.ru>
2012-01-17 16:46:28 +0700

    added debug code

--- DataConvertor/Program.cs
+++ DataConvertor/Program.cs
@@ -17,6 +17,9 @@ namespace AppMetrics.DataConvertor
 				if (args.Length == 0 || args.Length > 3)
 					throw new ApplicationException("Invalid command line args");
 
+				var listeners = new[] { new TextWriterTraceListener(Console.Out) };
+				Debug.Listeners.AddRange(listeners);
+
 				var dataPath = args[0];
 				var resPath = (args.Length >= 2) ? args[1] : ".";
 				var periodDays = (args.Length >= 3) ? TimeSpan.Parse(args[2]) : TimeSpan.MaxValue;
