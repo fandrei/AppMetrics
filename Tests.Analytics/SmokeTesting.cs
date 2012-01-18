5dde5994a677c7c171cc7a2c39905bc377803482
fandrei <andrei_f_11@mail.ru>
2012-01-18 18:22:36 +0700

    added test project stub

---
+++ Tests.Analytics/SmokeTesting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NUnit.Framework;
+
+namespace Tests.Analytics
+{
+	[TestFixture]
+	public class SmokeTesting
+	{
+		[Test]
+		public void SmokeTest()
+		{
+		}
+	}
+}
