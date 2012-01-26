e7aa5d6807ae65491003b507e9a4b5045025292a
fandrei <andrei_f_11@mail.ru>
2012-01-26 15:37:44 +0700

    move analytics code into a separate assembly

--- AppMetrics.AnalyticsSite/Properties/AssemblyInfo.cs
+++ AppMetrics.AnalyticsSite/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e58bb574-cd56-410b-a1c5-64568f4ff85a")]
+[assembly: Guid("a2874092-883b-4c74-989a-beabc367caaa")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
