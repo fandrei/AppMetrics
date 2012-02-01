ee42b1ea840381d821fbad5486013618f0fab5d1
fandrei <andrei_f_11@mail.ru>
2012-02-01 16:55:30 +0700

    refactoring

--- Tests.Analytics/UnitTesting.cs
+++ Tests.Analytics/UnitTesting.cs
@@ -15,19 +15,24 @@ namespace Tests.Analytics
 		[Test]
 		public void TestQuantilesCalculation()
 		{
-			var data1 = new decimal[] { 6, 47, 49, 15, 42, 41, 7, 39, 43, 40, 36 };
-			var res1 = Stats.CalculateSummaries(data1);
+			{
+				var data = new decimal[] { 6, 47, 49, 15, 42, 41, 7, 39, 43, 40, 36 };
+				var res = Stats.CalculateSummaries(data);
 
-			Assert.IsTrue(res1.LowerQuartile == 25.5M);
-			Assert.IsTrue(res1.Median == 40M);
-			Assert.IsTrue(res1.UpperQuartile == 42.5M);
+				Assert.IsTrue(res.LowerQuartile == 25.5M);
+				Assert.IsTrue(res.Median == 40M);
+				Assert.IsTrue(res.UpperQuartile == 42.5M);
+			}
 
-			var data2 = new decimal[] { 7, 15, 36, 39, 40, 41 };
-			var res2 = Stats.CalculateSummaries(data2);
+			{
+				var data = new decimal[] { 7, 15, 36, 39, 40, 41 };
+				var res = Stats.CalculateSummaries(data);
 
-			Assert.IsTrue(res2.LowerQuartile == 15M);
-			Assert.IsTrue(res2.Median == 37.5M);
-			Assert.IsTrue(res2.UpperQuartile == 40M);
+				Assert.IsTrue(res.LowerQuartile == 15M);
+				Assert.IsTrue(res.Median == 37.5M);
+				Assert.IsTrue(res.UpperQuartile == 40M);
+			}
+		}
 		}
 	}
 }
