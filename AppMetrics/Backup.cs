8df39152efabf81cdd0b40067f78e1103210c129
fandrei <andrei_f_11@mail.ru>
2012-03-28 14:32:12 +0700

    refactored S3 backup

--- AppMetrics/Backup.cs
+++ AppMetrics/Backup.cs
@@ -16,23 +16,27 @@ namespace AppMetrics
 		{
 			try
 			{
-				var now = DateTime.UtcNow;
-				var sessions = DataModel.DataSource.GetSessionsFromPath(AppSettings.DataStoragePath, TimeSpan.MaxValue);
-
-				foreach (var session in sessions)
 				{
-					if (now - session.LastUpdateTime < NonArchivePeriod)
-						continue;
+					var now = DateTime.UtcNow;
+					var sessions = DataModel.DataSource.GetSessionsFromPath(AppSettings.DataStoragePath, TimeSpan.MaxValue);
 
-					try
+					foreach (var session in sessions)
 					{
-						BackupFile(session.FileName);
-					}
-					catch (Exception exc)
-					{
-						reportLog(exc);
+						if (now - session.LastUpdateTime < NonArchivePeriod)
+							continue;
+
+						try
+						{
+							ArchiveFile(session.FileName);
+						}
+						catch (Exception exc)
+						{
+							reportLog(exc);
+						}
 					}
 				}
+
+				SyncAllToS3();
 			}
 			catch (Exception exc)
 			{
@@ -40,15 +44,7 @@ namespace AppMetrics
 			}
 		}
 
-		static void BackupFile(string fileName)
-		{
-			var zipFile = ArchiveFile(fileName);
-			SendFileToS3(zipFile);
-
-			//File.Delete(fileName);
-		}
-
-		public static string ArchiveFile(string fileName)
+		public static void ArchiveFile(string fileName)
 		{
 			var zipFileName = Path.ChangeExtension(fileName, ".zip");
 			using (var zipFile = new ZipFile(zipFileName))
@@ -61,34 +57,61 @@ namespace AppMetrics
 
 				zipFile.Save();
 			}
-			return zipFileName;
+
+			File.Delete(fileName);
 		}
 
-		public static void SendFileToS3(string fileName)
+		static void SyncAllToS3()
 		{
-			if (string.IsNullOrEmpty(AppSettings.Instance.AmazonAccessKey) || 
+			if (string.IsNullOrEmpty(AppSettings.Instance.AmazonAccessKey) ||
 					string.IsNullOrEmpty(AppSettings.Instance.AmazonSecretAccessKey))
 				return;
 
-			using (var client = new AmazonS3Client(AppSettings.Instance.AmazonAccessKey, AppSettings.Instance.AmazonSecretAccessKey))
+			using (var client = CreateAmazonS3Client())
 			{
-				using (var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-				{
-					var key = Path.GetFileName(fileName);
+				var storedFiles = client.ListObjects(new ListObjectsRequest { BucketName = AppMetricsBucketName });
+				var storedFilesDic = storedFiles.S3Objects.ToDictionary(storedObject => storedObject.Key);
 
-					client.PutObject(
-						new PutObjectRequest
-							{
-								BucketName = BucketName,
-								Key = key,
-								InputStream = stream,
-								ContentType = "application/zip"
-							});
+				foreach (var filePath in Directory.GetFiles(AppSettings.DataStoragePath, "*.zip", SearchOption.AllDirectories))
+				{
+					var key = Path.GetFileName(filePath);
+					S3Object storedObject;
+					if (storedFilesDic.TryGetValue(key, out storedObject))
+					{
+						var remoteLastModified = DateTime.Parse(storedObject.LastModified).ToUniversalTime();
+						var localFileInfo = new FileInfo(filePath);
+						if (localFileInfo.LastWriteTimeUtc <= remoteLastModified)
+							continue;
+					}
+
+					SendFileToS3(client, filePath);
 				}
 			}
 		}
 
-		private const string BucketName = "CityIndex.AppMetrics";
+		static void SendFileToS3(AmazonS3Client client, string fileName)
+		{
+			using (var stream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+			{
+				var key = Path.GetFileName(fileName);
+
+				client.PutObject(
+					new PutObjectRequest
+						{
+							BucketName = AppMetricsBucketName,
+							Key = key,
+							InputStream = stream,
+							ContentType = "application/zip"
+						});
+			}
+		}
+
+		static AmazonS3Client CreateAmazonS3Client()
+		{
+			return new AmazonS3Client(AppSettings.Instance.AmazonAccessKey, AppSettings.Instance.AmazonSecretAccessKey);
+		}
+
+		private const string AppMetricsBucketName = "CityIndex.AppMetrics";
 		private static readonly TimeSpan NonArchivePeriod = TimeSpan.FromDays(7);
 	}
 }
