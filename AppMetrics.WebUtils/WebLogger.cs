fb7bae8d764b0600fe5c8d65dad00da2ba23629f
fandrei <andrei_f_11@mail.ru>
2012-06-14 16:23:18 +0800

    improved logging

--- AppMetrics.WebUtils/WebLogger.cs
+++ AppMetrics.WebUtils/WebLogger.cs
@@ -11,18 +11,26 @@ namespace AppMetrics.WebUtils
 	{
 		public static void Report(object val)
 		{
-			var msg = val.ToString() + Environment.NewLine;
-			Trace.WriteLine(msg);
-
 			try
 			{
+				var text = val.ToString();
+				Trace.WriteLine(text);
+
 				var filePath = Path.Combine(WebUtil.AppDataPath, "log.txt");
-				File.AppendAllText(filePath, msg);
+
+				var time = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+				bool multiLineData = text.Contains('\n');
+				var buf = (multiLineData)
+					? string.Format("{0}\r\n{1}\r\n{2}\r\n{3}\r\n", time, Delimiter, text, Delimiter)
+					: string.Format("{0}\t{1}\r\n", time, text);
+				File.AppendAllText(filePath, buf);
 			}
 			catch (Exception exc)
 			{
 				Trace.WriteLine(exc);
 			}
 		}
+
+		static readonly string Delimiter = new string('-', 80);
 	}
 }
