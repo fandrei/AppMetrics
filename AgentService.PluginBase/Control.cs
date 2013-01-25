6fab7d2f1530f905edfe705342487e0475c532a0
fandrei <andrei_f_11@mail.ru>
2013-01-25 15:40:46 +0700

    check if master process is running (for plugins)

---
+++ AgentService.PluginBase/Control.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AppMetrics.AgentService.PluginBase
+{
+	class Control
+	{
+		public static bool IsMasterRunning
+		{
+			get
+			{
+				var processFile = Process.GetCurrentProcess().MainModule.FileName.ToLower();
+				if (processFile.EndsWith(".vshost.exe"))
+					return true; // always true when running from VS
+
+				var masterProcesses = Process.GetProcesses("AppMetrics.AgentService.exe");
+				return (masterProcesses.Length > 0);
+			}
+		}
+	}
+}
