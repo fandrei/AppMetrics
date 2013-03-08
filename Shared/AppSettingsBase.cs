fe8af1e745eb13be97ad591ef4bd1948079377f9
fandrei <andrei_f_11@mail.ru>
2013-03-08 19:08:43 +0700

    bugfix

--- Shared/AppSettingsBase.cs
+++ Shared/AppSettingsBase.cs
@@ -15,33 +15,29 @@ namespace AppMetrics.Shared
 		protected virtual void OnBeforeSave()
 		{ }
 
-		private static XmlSerializer _serializer;
 		private string _fileName;
 
-		public static T Load<T>(string fileName, string rootName = null)
+		public static T Load<T>(string fileName)
 			where T : AppSettingsBase, new()
 		{
 			T settings;
 
 			if (File.Exists(fileName))
 			{
-				if (_serializer == null)
-				{
-					if (string.IsNullOrEmpty(rootName))
-						rootName = typeof(T).Name;
-					var rootAttr = new XmlRootAttribute(rootName);
-					_serializer = new XmlSerializer(typeof(T), null, null, rootAttr, "");
-				}
+				// http://blogs.msdn.com/b/tess/archive/2006/02/15/532804.aspx
+				// beware of memory leak if you modify the next line
+				var serializer = new XmlSerializer(typeof(T));
 				using (var rd = new StreamReader(fileName))
 				{
-					settings = (T)_serializer.Deserialize(rd);
+					settings = (T)serializer.Deserialize(rd);
 				}
 			}
 			else
 				settings = new T();
 
-			settings.OnAfterLoad();
 			settings._fileName = fileName;
+
+			settings.OnAfterLoad();
 
 			return settings;
 		}
@@ -54,10 +50,10 @@ namespace AppMetrics.Shared
 			if (!Directory.Exists(directory))
 				Directory.CreateDirectory(directory);
 
-			var s = new XmlSerializer(GetType());
+			var serializer = new XmlSerializer(GetType());
 			using (var writer = new StreamWriter(fileName))
 			{
-				s.Serialize(writer, this);
+				serializer.Serialize(writer, this);
 			}
 		}
 
