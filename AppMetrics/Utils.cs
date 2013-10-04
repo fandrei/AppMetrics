bd302edc359f7017a2683e9ce68038fd78477a03
fandrei <andrei.faber@gmail.com>
2013-10-04 17:15:52 +0700

    fixed mutex usage

--- AppMetrics/Utils.cs
+++ AppMetrics/Utils.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Security.Principal;
 using System.Threading;
-using System.Web;
 
 namespace AppMetrics
 {
@@ -20,23 +20,58 @@ namespace AppMetrics
 	{
 		public MutexLocker(string id)
 		{
-			_mutex = new Mutex(false, id);
-
-			var allowEveryoneRule = new MutexAccessRule(
-				new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
-			var securitySettings = new MutexSecurity();
-			securitySettings.AddAccessRule(allowEveryoneRule);
-			_mutex.SetAccessControl(securitySettings);
+			var doesNotExist = false;
+			var unauthorized = false;
 
 			try
 			{
-				_hasHandle = _mutex.WaitOne(TimeSpan.FromSeconds(10), false);
-				if (_hasHandle == false)
-					throw new ApplicationException(string.Format("Can't lock mutex: {0}", id));
+				_mutex = Mutex.OpenExisting(id, MutexRights.Synchronize | MutexRights.Modify);
 			}
-			catch (AbandonedMutexException)
+			catch (WaitHandleCannotBeOpenedException)
 			{
-				_hasHandle = true;
+				doesNotExist = true;
+			}
+			catch (UnauthorizedAccessException ex)
+			{
+				unauthorized = true;
+			}
+
+			if (doesNotExist)
+			{
+				_mutex = new Mutex(false, id);
+
+				var allowEveryoneRule = new MutexAccessRule(
+					new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+				var securitySettings = new MutexSecurity();
+				securitySettings.AddAccessRule(allowEveryoneRule);
+				_mutex.SetAccessControl(securitySettings);
+			}
+			else if (unauthorized)
+			{
+				var tempMutex = Mutex.OpenExisting(id, MutexRights.ReadPermissions | MutexRights.ChangePermissions);
+				var securitySettings = tempMutex.GetAccessControl();
+
+				var user = Environment.UserDomainName + "\\" + Environment.UserName;
+
+				// the rule that denied the current user the right to enter and release the mutex must be removed
+				var rule = new MutexAccessRule(user, MutexRights.Synchronize | MutexRights.Modify, AccessControlType.Deny);
+				securitySettings.RemoveAccessRule(rule);
+
+				// Now grant the correct rights
+				var allowEveryoneRule = new MutexAccessRule(
+					new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+				securitySettings.AddAccessRule(allowEveryoneRule);
+				tempMutex.SetAccessControl(securitySettings);
+
+				_mutex = Mutex.OpenExisting(id, MutexRights.Synchronize | MutexRights.Modify);
+			}
+
+			var success = _mutex.WaitOne(TimeSpan.FromSeconds(10), false);
+			if (success == false)
+			{
+				_mutex.Dispose();
+				_mutex = null;
+				throw new ApplicationException(string.Format("Can't lock mutex (timed out): {0}", id));
 			}
 		}
 
@@ -44,15 +79,20 @@ namespace AppMetrics
 		{
 			if (_mutex != null)
 			{
-				if (_hasHandle)
+				try
+				{
 					_mutex.ReleaseMutex();
+				}
+				catch (Exception exc)
+				{
+					Trace.WriteLine(exc);
+				}
 
 				_mutex.Dispose();
 			}
 		}
 
 		private readonly Mutex _mutex;
-		private bool _hasHandle;
 	}
 
 }
